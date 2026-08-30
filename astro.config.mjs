17ebf2bfde33d01381b69cda6bf2436c2f06ca3f
svaa-dev <sotomayorvalerioadrian@gmail.com>
2026-08-30 06:58:53 -0500

    chore: client structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import react from '@astrojs/react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  srcDir: 'src/client',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
