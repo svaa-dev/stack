0644b41d4bdc15129fc40e2b719b3c81ca87ef8e
svaa-dev <sotomayorvalerioadrian@gmail.com>
2026-08-30 06:11:08 -0500

    chore: integrate Tailwind CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 })
