5912c8a176d9d26d65689723f85aba08b61ab4f4
svaa-dev <sotomayorvalerioadrian@gmail.com>
2026-09-07 07:55:01 -0500

    chore: reorder import

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,5 @@
-import { drizzle } from 'drizzle-orm/d1'
 import type { D1Database } from '@cloudflare/workers-types'
+import { drizzle } from 'drizzle-orm/d1'
 
 export const getDb = (binding: D1Database) => {
   return drizzle(binding)
--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,6 +1,6 @@
+import type { NewUser } from './schema'
 import type { D1Database } from '@cloudflare/workers-types'
 import { getDb } from '.'
-import type { NewUser } from './schema'
 import * as schema from './schema'
 
 export const insertUser = async (dbBinding: D1Database, user: NewUser) => {
